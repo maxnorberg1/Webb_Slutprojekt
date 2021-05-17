dddeafc65e2af536377874e03431bd4ab30ffb0a
maxnorberg1 <max.norberg@elev.ga.ntig.se>
2021-05-17 22:03:42 +0200

    Update app.js

--- webbshop/app.js
+++ webbshop/app.js
@@ -57,6 +57,7 @@ const calculateOrderAmount = items => {
   // Replace this constant with a calculation of the order's amount
   // Calculate the order total on the server to prevent
   // people from directly manipulating the amount on the client
+  
   return 1400;
 };
 
